01f9bd9d0a943838f70d746013302c4b77fe24d9
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-09-26 07:14:17 -0400

    Created portfolio and project page to display the portfolio and each of our projects

---
+++ src/pages/portfolio.js
@@ -0,0 +1,6 @@
+import { Project } from "../project";
+import { Portfolio } from "../portfolio";
+
+const portfolio = document.createElement("div");
+const name = document.createElement("input");
+const goal = document.createElement("input");
